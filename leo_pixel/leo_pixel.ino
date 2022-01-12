b9a37b9cbd46956de824a76a6cf0ff14b6b203b7
Kim HyunWoo <stories2@naver.com>
2022-01-12 15:39:32 +0900

    Update leo_pixel.ino

--- leo_pixel/leo_pixel.ino
+++ leo_pixel/leo_pixel.ino
@@ -1,21 +1,26 @@
 #include <Adafruit_NeoPixel.h>
 #define PIN            7                              // 제어 신호핀
 #define NUMPIXELS      30                            // 제어하고 싶은 LED 개수
+#define SHOW_LEN 5
  
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-int delayval = 500; // delay for half a second
+int delayval = 50; // delay for half a second
+int startPoint = 0;
  
 void setup() {
   pixels.begin(); // This initializes the NeoPixel library.
+//  pixels.setBrightness(100);
 }
  
 void loop() {
-  for(int i=0;i<NUMPIXELS;i++){
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
- 
-    pixels.show(); // This sends the updated pixel color to the hardware.
- 
-    delay(delayval); // Delay for a period of time (in milliseconds).
+//  for(int i = 0; i < NUMPIXELS; i ++) {
+//    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+//  }
+  for(int i = 0; i < NUMPIXELS; i ++) {
+    float scalar = sin(radians(360 / NUMPIXELS * i + 360 / NUMPIXELS * startPoint)) * 0.5 + 0.5;
+    pixels.setPixelColor(i, pixels.Color(196 * scalar, 165 * scalar, 66 * scalar));
   }
+  startPoint = (startPoint + 1) % NUMPIXELS;
+  pixels.show();
+  delay(delayval);
 }
