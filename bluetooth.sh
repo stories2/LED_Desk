a5526c0e7c78292ac4c3ac1364cbf4c473cee5cf
Kim HyunWoo <stories2@naver.com>
2022-01-15 00:05:05 +0900

    Update bluetooth.sh

--- bluetooth.sh
+++ bluetooth.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 hciconfig hci0 name 'led-controller'
-hciconfig hci0 name
+hciconfig hci0 down
+hciconfig hci0 up
 service bluetooth restart
+hciconfig hci0 name
 hciconfig hci0 piscan
