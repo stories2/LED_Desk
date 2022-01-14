0667d17937d12bc0d69ffbfa04d4df2ffdfe9b44
Kim HyunWoo <stories2@naver.com>
2022-01-15 00:25:48 +0900

    Update bluetooth.sh

--- bluetooth.sh
+++ bluetooth.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-hciconfig hci0 name 'led-controller'
+#hciconfig hci0 name 'led-controller'
 #hciconfig noauth
-hciconfig hci0 sspmode 1
-hciconfig hci0 down
-hciconfig hci0 up
-service bluetooth restart
-sleep 10
+#hciconfig hci0 sspmode 1
+#hciconfig hci0 down
+#hciconfig hci0 up
+#service bluetooth restart
+#sleep 10
 #hciconfig hci0 name
 #hciconfig hci0 piscan
 
@@ -16,4 +16,5 @@ discoverable on
 pairable on
 agent NoInputNoOutput
 default-agent 
+system-alias led-controller
 EOF
