63f8344b7311e914c77a8d70ef158f408f472b8f
Kim HyunWoo <stories2@naver.com>
2022-01-14 18:12:28 +0900

    Update leo_pixel.ino

--- leo_pixel/leo_pixel.ino
+++ leo_pixel/leo_pixel.ino
@@ -1,4 +1,5 @@
 #include <Adafruit_NeoPixel.h>
+
 #define PIN            7                              // 제어 신호핀
 #define NUMPIXELS      30                            // 제어하고 싶은 LED 개수
 #define SHOW_LEN 5
@@ -6,8 +7,10 @@
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 int delayval = 50; // delay for half a second
 int startPoint = 0;
+float now = 0;
  
 void setup() {
+  now = millis();
   pixels.begin(); // This initializes the NeoPixel library.
 //  pixels.setBrightness(100);
 }
@@ -22,5 +25,8 @@ void loop() {
   }
   startPoint = (startPoint + 1) % NUMPIXELS;
   pixels.show();
-  delay(delayval);
+
+  while ((millis() - now) < delayval) {
+  }
+  now = millis();
 }
