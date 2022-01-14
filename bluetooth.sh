c7ca0dc0d1bb267228ed197efe947aa8c0ef85a8
Kim HyunWoo <stories2@naver.com>
2022-01-15 00:20:20 +0900

    Update bluetooth.sh

--- bluetooth.sh
+++ bluetooth.sh
@@ -6,5 +6,13 @@ hciconfig hci0 sspmode 1
 hciconfig hci0 down
 hciconfig hci0 up
 service bluetooth restart
-hciconfig hci0 name
-hciconfig hci0 piscan
+#hciconfig hci0 name
+#hciconfig hci0 piscan
+
+bluetoothctl <<EOF
+power on
+discoverable on
+pairable on
+agent NoInputNoOutput
+default-agent 
+EOF
