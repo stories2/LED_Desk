2f80be52250f6b717ef9e401b97239e264799158
Kim HyunWoo <stories2@naver.com>
2022-01-15 00:50:56 +0900

    Update bluetooth-install.sh

--- bluetooth-install.sh
+++ bluetooth-install.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install bluez-tools -y
 
-echo "* 1234" >> /etc/bluetooth/pin.conf
+echo "* 1234" > /etc/bluetooth/pin.conf
 
 chown root:root /etc/bluetooth/pin.conf
 chmod 600 /etc/bluetooth/pin.conf
