e36c9cc1dc97ec0ec008c426a85b887299f6f7e0
Kim HyunWoo <stories2@naver.com>
2022-01-15 01:00:43 +0900

    Update bluetooth-install.sh

--- bluetooth-install.sh
+++ bluetooth-install.sh
@@ -6,3 +6,10 @@ echo "* 1234" > /etc/bluetooth/pin.conf
 
 chown root:root /etc/bluetooth/pin.conf
 chmod 600 /etc/bluetooth/pin.conf
+
+cp bt-agent.service /etc/systemd/system/
+
+systemctl daemon-reload
+systemctl enable bt-agent
+systemctl restart bt-agent
+systemctl status bt-agent.service
