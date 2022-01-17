545be9326fd59478741056307af58d5f9353ab51
Kim HyunWoo <stories2@naver.com>
2022-01-17 17:31:49 +0900

    Update app.py

--- backend/src/app.py
+++ backend/src/app.py
@@ -2,6 +2,23 @@ from flask import Flask, request, jsonify
 import RPi.GPIO as GPIO
 import board
 import neopixel
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask(__name__)
 NUMPIXELS = 30
