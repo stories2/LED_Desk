ee6a6cf67cce53c70073cc0bea2c9e3a19c8f62b
Kim HyunWoo <stories2@naver.com>
2022-01-17 17:12:15 +0900

    Update app.py

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,7 +1,36 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import RPi.GPIO as GPIO
+import board
+import neopixel
 
 app = Flask(__name__)
+NUMPIXELS = 30
+GPIO.setwarnings(True)
+GPIO.setmode(GPIO.BCM) 
+pixels = neopixel.NeoPixel(board.D18, NUMPIXELS)
 
 @app.route("/")
 def hello_world():
     return "<p>Hell World!!</p>"
+
+@app.route("/led", methods=['POST'])
+def change_led_color():
+    try:
+        payload = request.json
+        for i in range(NUMPIXELS):
+            pixels[i] = (payload["R"], payload["G"], payload["B"])
+        pixels.show()
+        return jsonify({
+            "result": "ok"
+        })
+    except Exception as e:
+        return jsonify({
+            "result": "fail",
+            "message": str(e)
+        })
+    # if request.is_json:
+    #     content = request.get_json(silent=True)
+    #     return f'R: {str(content['R'])} G: {str(content['G'])} B: {str(content['B'])}'
+    # else:
+    #     return 'fail'
+    
