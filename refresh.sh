5c268124840d857c116e7f4abf34dfcea6c9d297
Kim HyunWoo <stories2@naver.com>
2022-01-17 18:35:12 +0900

    add builder

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
 git pull
+
+docker build -t led_frontend ./frontend/
+docker run  --name led_frontend -v ./backend/src/templates:/app/dist/spa led_frontend 
+
 sudo docker-compose build
 sudo docker-compose up -d
