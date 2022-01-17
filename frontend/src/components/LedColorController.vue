b46a09177922604421d358bf38cb376504bdada0
Kim HyunWoo <stories2@naver.com>
2022-01-17 22:47:42 +0900

    Update LedColorController.vue

--- frontend/src/components/LedColorController.vue
+++ frontend/src/components/LedColorController.vue
@@ -1,17 +1,31 @@
 <template>
-    <div class="q-pa-md">
-        <q-badge color="grey-3" text-color="black" class="q-mb-sm">
-            {{ rgb }}
-        </q-badge>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="q-pa-md">
+                    <q-badge color="grey-3" text-color="black" class="q-mb-sm">
+                        {{ rgb }}
+                    </q-badge>
 
-        <q-color
-            :model-value="rgb"
-            @change="val => onColorChange(val)"
-            style="max-width: 250px"
-            no-footer
-            no-header-tabs
-        />
-  </div>
+                    <q-color
+                        :model-value="rgb"
+                        @change="val => onColorChange(val)"
+                        style="max-width: 250px"
+                        no-footer
+                        no-header-tabs
+                    />
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col text-center">
+                <q-btn outline rounded color="primary"
+                    :label="lightSwitch"
+                    @click="onTurnOnOffBtnClicked()"
+                />
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -27,11 +41,11 @@ export default defineComponent({
     name: 'CompositionComponent',
     setup() {
         const $q = useQuasar()
-        let rgb = ref('rgb(0, 0, 0)')
+        let rgb = ref('rgb(0,0,0)')
+        let lightSwitch = ref('Turn on')
 
-        const onColorChange = (color: string) => {
-            rgb.value = color
-            const colors = /rgb\(([\d]{1,3})\,([\d]{1,3})\,([\d]{1,3})\)/g.exec(rgb.value)
+        const sendColorChangeReq = (rgb: string) => {
+            const colors = /rgb\(([\d]{1,3})\,([\d]{1,3})\,([\d]{1,3})\)/g.exec(rgb)
             if (colors && colors.length === 4) {
                 const payload = {
                     R: Number(colors[1]),
@@ -67,13 +81,42 @@ export default defineComponent({
                     console.error(response)
                 });
             } else {
-
+                $q.notify({
+                    message: 'Unknown color detected.',
+                    timeout: 1000,
+                })
             }
+        }
+
+        const onTurnOnOffBtnClicked = () => {
+            if (rgb.value === 'rgb(0,0,0)') {
+                lightSwitch.value = 'Turn off'
+                rgb.value = 'rgb(255,255,255)'
+            } else {
+                lightSwitch.value = 'Turn on'
+                rgb.value = 'rgb(0,0,0)'
+            }
+            
+            sendColorChangeReq(rgb.value)
+        }
+
+        const onColorChange = (color: string) => {
+            rgb.value = color
+            
+            if (rgb.value === 'rgb(0,0,0)') {
+                lightSwitch.value = 'Turn on'
+            } else {
+                lightSwitch.value = 'Turn off'
+            }
+            
+            sendColorChangeReq(rgb.value)
         }
 
         return { 
             rgb,
-            onColorChange
+            onColorChange,
+            lightSwitch,
+            onTurnOnOffBtnClicked
         };
     },
 });
